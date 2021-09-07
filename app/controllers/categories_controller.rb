d8a28f5abe77d61aae2b1bd0ac72ed42ada0c688
“nirmal-vaja” <nirmalvaja123@gmail.com>
2021-09-07 16:56:52 +0530

    Admin User Requirement and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,11 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
   end
 
   def index
-    # @categories = Category.all
     @categories = Category.paginate(page: params[:page], per_page: 4)
   end
 
@@ -27,6 +27,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name )
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only Admins can perform that action."
+      redirect_to categories_path
+    end
   end
 
 end
